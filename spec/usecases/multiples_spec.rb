ba8d3d1fcd27041db5391f1fd97cfb36235259d2
Naiguel Santos <naiguel.santos@objective.com.br>
2021-08-02 18:35:06 -0300

    test: context

--- spec/usecases/multiples_spec.rb
+++ spec/usecases/multiples_spec.rb
@@ -1,8 +1,8 @@
 require './app/entity/sum'
 
 RSpec.describe Entity::Sum do
-  context 'múltiplos de 3 ou 5 de números naturais abaixo de 1000' do
-    it 'deve somar corretamente' do
+  context 'multiples of 3 or 5, less then 1000' do
+    it 'should be the correct result' do
       soma = 0
 
       expected_result = 233_168
@@ -27,23 +27,17 @@ RSpec.describe Entity::Sum do
     end
   end
 
-  it 'soma de todos os números múltiplos de 3 e 5 de números naturais abaixo de 1000' do
-    soma = 0
+  context 'multiples of 3 and 5, less then 1000' do
+    it 'should be the correct result' do
+      soma = 0
 
-    expected_result = 33_165
+      expected_result = 33_165
 
-    Entity::Sum.multiples.each do |item|
-      soma = Entity::Sum.multiple_and(soma, item, 3, 5)
+      Entity::Sum.multiples.each do |item|
+        soma = Entity::Sum.multiple_and(soma, item, 3, 5)
+      end
+
+      expect(soma).to eq(expected_result)
     end
-
-    expect(soma).to eq(expected_result)
-  end
-
-  it 'soma de todos os números múltiplos de 3 e 5 de números naturais abaixo de 1000' do
-    soma = 12
-
-    soma = Entity::Sum.multiple_and(soma)
-
-    expect(soma).to eq(12)
   end
 end
